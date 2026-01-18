20a6d5a70abcbbf153f58670a324b4121e99a530
unknown <sseis2349@users.noreply.github.com>
2026-01-17 21:43:31 -0800

    rebuild from scratch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,19 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-     
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-
-              Welcome to AniSlides
-
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          
+          Learn React
         </a>
       </header>
     </div>
