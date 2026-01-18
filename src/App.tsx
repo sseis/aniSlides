cd68b8e7158a68b2ca465bfc2035a829d2f656a4
unknown <sseis2349@users.noreply.github.com>
2026-01-17 18:11:32 -0800

    Initial testing

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+     
+        <p>
+        </p>
+
+              Welcome to AniSlides
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
